040107aae27b2edbca458ea7a5d3ba09cbb791b6
Reydescel Herrera <reydi1@hotmail.com>
2023-03-11 13:25:36 -0600

    Adding a new Color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'orange': '#fFB43A',
+      'blue': '#272833',
+      'blue-dark': '#1C1C27',
+      'red': '#E94141',
+      'white': '#FCFCFC',
+      'gray-dark': '#2c3e50'
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      serif: ['Open Sans', 'serif'],
+    },
   },
   plugins: [],
 }
